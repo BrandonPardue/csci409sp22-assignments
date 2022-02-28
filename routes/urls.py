31b4c595fe8fc49e85d830f11bd60c94af84ba07
BrandonPardue <blpardue@coastal.edu>
2022-02-28 15:12:16 -0500

    Initial Commit

---
+++ routes/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+
+
+    path('/', views.index),
+
+    path('/search/<str:origin>/<str:destination>', views.route_search)
+
+]
---
+++ tickets/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from. import views
+
+urlpatterns = [
+
+    path('/', views.index),
+
+    path('/<int:confirmation_number>', views.ticket_search),
+
+
+]
