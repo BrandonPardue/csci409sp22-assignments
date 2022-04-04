d90a32ef0706e978f4af383209a9de871272cfa5
BrandonPardue <blpardue@coastal.edu>
2022-04-04 16:05:52 -0400

    updated for week 10 assignment

--- airports/views.py
+++ airports/views.py
@@ -1,13 +1,14 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 from .models import Airport
 
 def index(request):
-    airports = Airport.objects.all()
-    airport_list = ', '.join([a.airport_code for a in airports])
-    return HttpResponse('Showing all airports: ' + airport_list);
-
-
+    airports = Airport.objects.all()  # gather data from the database
+    context = {'airports': airports}  # assign it to a context variable
+    return render(request, 'airports/index.html', context)  # render the template with the context data
 def airport_info(request, airport_code):
-    airport = Airport.objects.get(airport_code=airport_code)
-    return HttpResponse('Showing info for airport: ' + airport.name + "- " + airport.airport_code)
+    airport = Airport.objects.get(airport_code=airport_code)  # gather the data matching the necessary parameters
+    context = {'airport': airport}  # attach line 10's result to a context variable
+    return render(request, 'airports/airport.html', context)  # render the template using the context variable
+
 
